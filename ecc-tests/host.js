8935b5b11f811cd4b22182756a292b52262ddd13
Jian Weng <jian.weng@ucla.edu>
2023-04-16 16:16:05 -0700

    ok. i figure out how to write to screen

---
+++ ecc-tests/host.js
@@ -0,0 +1,34 @@
+fs = require('fs');
+
+wasm_binary = process.argv[2]
+
+fd = fs.readFileSync(wasm_binary, undefined)
+
+function print_int(x) {
+  process.stdout.write(x.toString())
+}
+
+memory = null
+i8view = null
+
+function print_string(offset, len) {
+  for (i = 0; i < len; ++i) {
+    process.stdout.write(String.fromCharCode(i8view[offset + i]))
+  }
+}
+
+imports = {
+  host: {
+    print_int: print_int,
+    print_string: print_string
+  }
+}
+
+WebAssembly.instantiate(fd, imports).then(function (result) {
+
+  memory = result.instance.exports.memory
+  i8view = new Int8Array(memory.buffer)
+
+  result.instance.exports.run()
+})
+
