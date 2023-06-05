4760089a21d935239122dd4310e66290cb122947
Jian Weng <jian.weng@ucla.edu>
2023-06-05 10:08:51 -0700

    forward subrepos

--- ecc-tests/host.js
+++ ecc-tests/host.js
@@ -17,24 +17,29 @@ function print_int(x) {
 memory = null
 i8view = null
 
-function print_string(offset, len) {
+function print_str(offset, len) {
   for (i = 0; i < len; ++i) {
     process.stdout.write(String.fromCharCode(i8view[offset + i]))
   }
 }
 
+__linear_memory = new WebAssembly.Memory({initial: 65536})
+__stack_pointer = new WebAssembly.Global({value: "i32", mutable: true}, 0)
+
 imports = {
-  host: {
+  env: {
     print_int: print_int,
-    print_string: print_string
+    print_str: print_str,
+    __linear_memory: __linear_memory,
+    __stack_pointer: __stack_pointer,
   }
 }
 
 WebAssembly.instantiate(fd, imports).then(function (result) {
 
-  memory = result.instance.exports.memory
-  i8view = new Int8Array(memory.buffer)
+  // memory = result.instance.exports.memory
+  i8view = new Int8Array(__linear_memory.buffer)
 
-  result.instance.exports.run()
+  result.instance.exports.main(0, 0)
 })
 
